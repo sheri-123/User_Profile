1a6517bb49710c8975252fd53a14d37b9b4183b2
Shaheryar Ali <sharybro654@gmail.com>
2025-08-22 14:07:22 +0500

    Fix: Use python3 command in build script

--- build_files.sh
+++ build_files.sh
@@ -2,11 +2,11 @@
 
 # Build the project
 echo "Building the project..."
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt # CHANGED
 
 echo "Make Migrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3 manage.py makemigrations --noinput # CHANGED
+python3 manage.py migrate --noinput # CHANGED
 
 echo "Collect Static..."
-python manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear # CHANGED
